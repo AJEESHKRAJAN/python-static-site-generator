a3e5fe332acdeeca4acceda7f07bfc2de3915cac
Ajeesh K Rajan <AJEESHKRAJAN@users.noreply.github.com>
2020-07-15 16:37:23 +0100

    Parsers Commit

--- ssg.py
+++ ssg.py
@@ -1,13 +1,13 @@
 import typer
-import ssg.parsers
 from ssg.site import Site
+import ssg.parsers
 
 
 def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": ssg.parsers.ResourceParser()
+        "parsers": [ssg.parsers.ResourceParser(), ],
     }
 
     Site(**config).build()
